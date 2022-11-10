4bfa5047821abd90a0f64daf077dba3d63ec81b6
shoxrux071 <begratefuller071@gmail.com>
2022-11-11 04:39:07 +0500

    first commit

---
+++ creating-multiple-authentication-providers/src/main/java/uz/shox/creatingmultipleauthenticationproviders/CreatingMultipleAuthenticationProvidersApplication.java
@@ -0,0 +1,13 @@
+package uz.shox.creatingmultipleauthenticationproviders;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreatingMultipleAuthenticationProvidersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CreatingMultipleAuthenticationProvidersApplication.class, args);
+    }
+
+}
