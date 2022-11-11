a32be1eb63759cfd4319a10cbaaca267a7c2a8c4
shoxrux071 <begratefuller071@gmail.com>
2022-11-12 04:30:50 +0500

    method-level-Authorization

---
+++ method-level-authorization/src/main/java/uz/shoz/methodlevelauthorization/MethodLevelAuthorizationApplication.java
@@ -0,0 +1,13 @@
+package uz.shoz.methodlevelauthorization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MethodLevelAuthorizationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MethodLevelAuthorizationApplication.class, args);
+    }
+
+}
---
+++ method-level-authorization/src/test/java/uz/shoz/methodlevelauthorization/MethodLevelAuthorizationApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.shoz.methodlevelauthorization;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MethodLevelAuthorizationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
