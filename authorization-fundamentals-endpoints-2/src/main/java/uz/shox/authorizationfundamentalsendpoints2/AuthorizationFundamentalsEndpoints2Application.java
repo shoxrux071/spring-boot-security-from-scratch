7309643ee39d65e2cdfea85f7f4dd1b1a4a15cbc
shoxrux071 <begratefuller071@gmail.com>
2022-11-11 23:23:17 +0500

    authorization endpoint 2

---
+++ authorization-fundamentals-endpoints-2/src/main/java/uz/shox/authorizationfundamentalsendpoints2/AuthorizationFundamentalsEndpoints2Application.java
@@ -0,0 +1,13 @@
+package uz.shox.authorizationfundamentalsendpoints2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthorizationFundamentalsEndpoints2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthorizationFundamentalsEndpoints2Application.class, args);
+    }
+
+}
---
+++ authorization-fundamentals-endpoints-2/src/test/java/uz/shox/authorizationfundamentalsendpoints2/AuthorizationFundamentalsEndpoints2ApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.shox.authorizationfundamentalsendpoints2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthorizationFundamentalsEndpoints2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
